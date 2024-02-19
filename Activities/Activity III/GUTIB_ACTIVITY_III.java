17e4ebbd2996efda91beb7be2a2841589964aac3
JOHNLOYD-svg <156911022+JOHNLOYD-svg@users.noreply.github.com>
2024-02-19 18:57:22 +0800

    Update GUTIB_ACTIVITY_III.java

--- Activities/Activity III/GUTIB_ACTIVITY_III.java
+++ Activities/Activity III/GUTIB_ACTIVITY_III.java
@@ -5,7 +5,7 @@ public class GUTIB_ACTIVITY_III {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int temp;
-        System.out.print("Enter your emperatur: ");
+        System.out.print("Enter your temperature: ");
         temp = input.nextInt();
         if (temp >= 100) {
             System.out.println("Boioling");
